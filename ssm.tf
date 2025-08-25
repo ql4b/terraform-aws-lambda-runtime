bcee15207af4616332708cf61861b748e35eb8e6
Carlo D'Ambrosio <carlo@ql4b.net>
2025-08-25 04:38:27 +0200

    feat: initial version (#1)

---
+++ ssm.tf
@@ -0,0 +1,7 @@
+resource "aws_ssm_parameter" "image" {
+  name  = "${local.ssm_prefix}/image"
+  type  = "String"
+  value = "${module.ecr.repository_url}:${local.deploy_tag}"
+  
+  tags = module.this.tags
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "deploy_tag" {
+  type        = string
+  description = "Docker image tag for deployment"
+  default     = "latest"
+}
